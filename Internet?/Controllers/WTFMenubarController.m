4b32449e6076112434e2bf335e2f16e60d37807e
Shady <shady@shadyproject.net>
2014-10-17 18:17:20 -0700

    make add/remove startup item menu work

--- Internet?/Controllers/WTFMenubarController.m
+++ Internet?/Controllers/WTFMenubarController.m
@@ -8,9 +8,11 @@
 
 #import "WTFMenubarController.h"
 #import "WTFSoundController.h"
+#import "WTFLoginItemController.h"
 
 @interface WTFMenubarController ()
 @property (nonatomic, strong) WTFSoundController *soundController;
+@property (nonatomic, strong) WTFLoginItemController *loginItemController;
 @end
 
 @implementation WTFMenubarController
@@ -18,6 +20,9 @@
 -(id)init {
     self = [super init];
     if (self) {
+        _soundController = [[WTFSoundController alloc] init];
+        _loginItemController = [[WTFLoginItemController alloc] init];
+        
         _statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:24.0];
         _statusItem.image = [NSImage imageNamed:@"BarIcon"];
         _statusItem.highlightMode = YES;
@@ -27,12 +32,17 @@
         [[menu addItemWithTitle:@"You are the ones" action:@selector(playYouAre:) keyEquivalent:@""] setTarget:self];
         [[menu addItemWithTitle:@"Eat" action:@selector(playEat:) keyEquivalent:@""] setTarget:self];
         [menu addItem:[NSMenuItem separatorItem]];
-        //[[menu addItemWithTitle:@"Window" action:@selector(openWindow:) keyEquivalent:@""] setTarget:self];
+        
+        NSMenuItem *item = [[NSMenuItem alloc] initWithTitle:@"Launch on startup" action:@selector(setStartupItem:) keyEquivalent:@""];
+        item.target = self;
+        item.state = [_loginItemController appExistsAsLoginItem] ? NSOnState : NSOffState;
+        [menu addItem:item];
+        
+        [menu addItem:[NSMenuItem separatorItem]];
         [[menu addItemWithTitle:@"Quit" action:@selector(quit:) keyEquivalent:@""] setTarget:self];
 
         _statusItem.menu = menu;
         
-        _soundController = [[WTFSoundController alloc] init];
     }
     
     return self;
@@ -57,6 +67,17 @@
 
 - (void)quit:(id)sender {
     [[NSApplication sharedApplication] terminate:sender];
+}
+
+- (void)setStartupItem:(id)sender {
+    if ([self.loginItemController appExistsAsLoginItem]) {
+        [self.loginItemController removeAppFromLoginItems];
+    } else {
+        [self.loginItemController addAppToLoginItems];
+    }
+    
+    NSMenuItem *item = [self.statusItem.menu itemWithTitle:@"Launch on startup"];
+    item.state = [self.loginItemController appExistsAsLoginItem] ? NSOnState : NSOffState;
 }
 
 - (void)dealloc {
